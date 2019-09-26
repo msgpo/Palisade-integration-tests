8b1f009e0d34268e1f9e7a6428972b2f0b7e5eaf
dev-958 <wolfie.acb@gmail.com>
2019-09-26 11:13:34 +0100

    to CommandLineRunner

--- palisade-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/PalisadeTestApplication.java
+++ palisade-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/PalisadeTestApplication.java
@@ -1,19 +1,28 @@
 package uk.gov.gchq.palisade.integrationtests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class PalisadeTestApplication {
+public class PalisadeTestApplication implements CommandLineRunner {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(PalisadeTestApplication.class);
 
     /**
      * Application entry point
      * @param args from the command line
      */
     public static void main(final String[] args) {
-        new SpringApplicationBuilder(PalisadeTestApplication.class).web(WebApplicationType.SERVLET)
+         new SpringApplicationBuilder(PalisadeTestApplication.class).web(WebApplicationType.NONE)
                 .run(args);
     }
 
+    @Override
+    public void run(String... args) {
+        LOGGER.info("Starting {}...", PalisadeTestApplication.class.getSimpleName());
+    }
 }
