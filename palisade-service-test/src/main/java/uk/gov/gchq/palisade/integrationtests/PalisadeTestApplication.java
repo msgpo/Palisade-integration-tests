6022fa42a316dca8012f36bb54d0f50db479f06e
dev-958 <wolfie.acb@gmail.com>
2019-09-27 16:53:05 +0100

    simple health test

--- palisade-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/PalisadeTestApplication.java
+++ palisade-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/PalisadeTestApplication.java
@@ -2,27 +2,35 @@ package uk.gov.gchq.palisade.integrationtests;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import uk.gov.gchq.palisade.service.palisade.PalisadeApplication;
 
 @SpringBootApplication
-public class PalisadeTestApplication implements CommandLineRunner {
+public class PalisadeTestApplication {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(PalisadeTestApplication.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(PalisadeTestApplication.class);
 
     /**
      * Application entry point
      * @param args from the command line
      */
     public static void main(final String[] args) {
-         new SpringApplicationBuilder(PalisadeTestApplication.class).web(WebApplicationType.NONE)
+         new SpringApplicationBuilder(PalisadeApplication.class).web(args.length == 0 ? WebApplicationType.SERVLET : WebApplicationType.NONE)
                 .run(args);
     }
 
-    @Override
-    public void run(String... args) {
-        LOGGER.info("Starting {}...", PalisadeTestApplication.class.getSimpleName());
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            LOGGER.info("Starting {}...", PalisadeTestApplication.class.getSimpleName());
+        };
     }
+
 }
---
+++ palisade-service-test/src/test/java/uk/gov/gchq/palisade/integrationtests/PalisadeHealthTest.java
@@ -0,0 +1,27 @@
+package uk.gov.gchq.palisade.integrationtests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PalisadeHealthTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void registerRequestTest() {
+        final String health = this.restTemplate.getForObject("/actuator/health", String.class);
+        assertThat(health, is(equalTo("{\"status\":\"UP\"}")));
+    }
+
+}
