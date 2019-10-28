eb361c96cad449b187d1ae8d3c09f30d76a949f6
developer01189998819991197253 <ChrisCoDev@gmail.com>
2019-10-28 17:06:04 +0000

    PAL-163-added checkstyle, updated palisade-services-test pom and root pom

--- palisade-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/PalisadeTestApplication.java
+++ palisade-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/PalisadeTestApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import uk.gov.gchq.palisade.service.palisade.PalisadeApplication;
-import uk.gov.gchq.palisade.service.palisade.config.ApplicationConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "uk.gov.gchq.palisade.service.palisade.web")
