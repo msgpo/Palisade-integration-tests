b52c67558192d6b184e9be3d52d7411c281f174b
dev930018 <dev930018@gmail.com>
2019-11-13 11:17:26 +0000

    PAL-234 Removed unused import

--- cache-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/cache/service/SimpleCacheService.java
+++ cache-service-test/src/main/java/uk/gov/gchq/palisade/integrationtests/cache/service/SimpleCacheService.java
@@ -40,7 +40,6 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 
 /**
